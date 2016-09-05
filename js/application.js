f7b990e7d55bbba11b0e676ade1a8ccc17bbb203
Simon Anthony <github@torty.org.uk>
2016-09-05 18:10:50 +1000

    Broken it
    Used derived x offset and position absolute / fixed… doesn’t work.
    Can’t get the hoghts right for different sized screens.

--- js/application.js
+++ js/application.js
@@ -1,131 +1,148 @@
-					jQuery(document).ready(function($) {
+					jQuery(document).ready(function ($) {
 
-				$(function() {
-					console.log("THING called");
-					$('a[href*="#"]:not([href="#"])').click(function() {
-						//hijacks the anchor click action and does this >---
-						move_this = this.hash;
-						console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   link " + move_this + " clicked");
-						sra_menu(move_this);
-						closeMenu(); // closes mobile menu - even if already closed
-					});
-				});
+						$(function () {
+							console.log("THING called");
+							$('a[href*="#"]:not([href="#"])').click(function () {
+								//hijacks the anchor click action and does this >---
+								move_this = this.hash;
+								console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   link " + move_this + " clicked");
+								sra_menu(move_this);
+								closeMenu(); // closes mobile menu - even if already closed
+							});
+						});
 
-				function sra_menu(move_this) {
-					displayed_content = move_this;
-					var offset = 0;
-					display_here_top = $(top_of_site).offset().top;
-					displayed_content_top = $(displayed_content).offset().top;
-					console.log(displayed_content + " top is " + displayed_content_top);
-					move_here_y = (displayed_content_top - display_here_top);
-					console.log("move_here_y= " + move_here_y);
+						function sra_menu(move_this) {
+							displayed_content = move_this;
+							var offset = 0;
+							display_here_top = $(top_of_site).offset().top;
+							displayed_content_top = $(displayed_content).offset().top;
+							
+							pageWidth=$( document ).width();
+							
+							displayed_content_width=$(displayed_content ).width();
+								//console.log(displayed_content_left + " left is " + displayed_content_left);
+							
+							displayed_content_left = $(displayed_content).offset().left;
+							
+							console.log(pageWidth + " pageWidth " + displayed_content_width+"displayed_content_width");
+							
+							left_offset=(pageWidth-displayed_content_width)/2;
+							
+							move_here_y = (displayed_content_top - display_here_top);
+							//move_here_x = (displayed_content_left - displayed_content_left);
+							
+							//console.log("move_here_x= " + move_here_x);
+							console.log("move_here_y= " + move_here_y);
 
 
-					//resets last moved section if it has been moved
-					if (typeof last_content != 'undefined') {
-						console.log("last_content = " + last_content);
-						if (displayed_content != last_content) {
-							console.log(" -------------  transform state of last_content" + last_content + " = " + $(last_content).css("transform"));
-							$(last_content).css("transform", "none");
-							$(mask).css("opacity","0");
-							console.log("RESETTING");
-							console.log(" #############  RESETTING transform state of last_content" + last_content + " = " + $(last_content).css("transform"));
+							//resets last moved section if it has been moved
+							if (typeof last_content != 'undefined') {
+								console.log("last_content = " + last_content);
+								if (displayed_content != last_content) {
+									console.log(" -------------  transform state of last_content" + last_content + " = " + $(last_content).css("transform"));
+									$(last_content).css("transform", "none");
+									$(mask).css("opacity", "0");
+									console.log("RESETTING");
+									console.log(" #############  RESETTING transform state of last_content" + last_content + " = " + $(last_content).css("transform"));
+									last_content = displayed_content;
+									console.log(" after last_content = displayed_content = " + last_content);
+								}
+							} else {
+								console.log("last_content NOT SET")
+							};
+
+
+							console.log(" #############  transform state of " + displayed_content + " = " + $(displayed_content).css("transform"));
+
+
+							if ($(displayed_content).css("transform") == 'none') {
+								$(displayed_content).css("position", "absolute");
+								//$(displayed_content).css("border-left", "25vw");
+								move_here_y=-97;
+								$(displayed_content).css("transform", "translate("+left_offset+"px,"+move_here_y+"px)"); // moves current piece up from original place to just under menu at the top
+								$(displayed_content).css("z-index", "10");
+								$(mask).css("opacity", "0.5");
+								$(wrapper).css("overflow-y", "scroll");
+
+								$("body").css("overflow-y", "hidden");
+
+							} else {
+								$("body").css("overflow-y", "scroll");
+								$(wrapper).css("overflow-y", "auto");
+								$(displayed_content).css("transform", "none");
+								$(mask).css("opacity", "0");
+								console.log("reset?");
+								$(displayed_content).css("z-index", "1");
+								$(displayed_content).css("position", "relative");
+							}
 							last_content = displayed_content;
-							console.log(" after last_content = displayed_content = " + last_content);
 						}
-					} else {
-						console.log("last_content NOT SET")
-					};
+					});
+					var open = false;
 
+					function onClickMenu() {
+						console.log("onClickMenu called");
+						console.log("open = " + open);
 
-					console.log(" #############  transform state of " + displayed_content + " = " + $(displayed_content).css("transform"));
-
-
-					if ($(displayed_content).css("transform") == 'none') {
-						$(displayed_content).css("transform", "translate(0px,-" + move_here_y + "px)"); // moves current piece up from original place to just under menu at the top
-
-						$(displayed_content).css("z-index", "10");
-						$(mask).css("opacity","0.5");
-						$(wrapper).css("overflow-y", "scroll");
-
-							$("body").css("overflow-y", "hidden");
-
+						if (open == true) {
+							closeMenu();
 						} else {
-							$("body").css("overflow-y", "scroll");
-							$(wrapper).css("overflow-y", "auto");
-							$(displayed_content).css("transform", "none");
-							$(mask).css("opacity","0");
-							console.log("reset?");
-							$(displayed_content).css("z-index", "1");
+							openMenu();
 						}
-						last_content = displayed_content;
+						//open = !open;
+						//console.log("open="+open);
 					}
-});
-var open = false;
 
-function onClickMenu() {
-	console.log("onClickMenu called");
-	console.log("open = "+open);
+					function onClickCover() {
+						console.log("onClickCover called");
+						console.log("open = " + open);
+						closeMenu();
+					}
 
-	if (open == true) {
-		closeMenu();
-	} else {
-		openMenu();
-	}
-	//open = !open;
-	//console.log("open="+open);
-}
+					var scrollPos;
 
-function onClickCover() {
-	console.log("onClickCover called");
-	console.log("open = "+open);
-	closeMenu();
-}
+					function openMenu() {
+						console.log("openMenu called");
 
-var scrollPos;
+						scrollPos = jQuery('body').scrollTop();
+						jQuery('.cover').animate({
+							left: '80%'
+						});
+						jQuery('.mobile-menu-bar').animate({
+							left: '80%'
+						});
+						//jQuery('.off-screen-left').css('display', 'block');
+						jQuery('.off-screen-left').animate({
+							left: '0%'
+						});
+						jQuery('.outer').css({
+							position: 'fixed',
+							top: -(document.body.scrollTop)
+						});
+						jQuery(".body").on("touchmove", false);
+						jQuery('.cover').fadeIn();
+						jQuery('.fadeout').fadeOut();
+						open = true;
+					}
 
-function openMenu() {
-	console.log("openMenu called");
+					function closeMenu() {
 
-	scrollPos = jQuery('body').scrollTop();
-	jQuery('.cover').animate({
-		left: '80%'
-	});
-	jQuery('.mobile-menu-bar').animate({
-		left: '80%'
-	});
-	//jQuery('.off-screen-left').css('display', 'block');
-	jQuery('.off-screen-left').animate({
-		left: '0%'
-	});
-	jQuery('.outer').css({
-		position: 'fixed',
-		top: -(document.body.scrollTop)
-	});
-	jQuery(".body").on("touchmove", false);
-	jQuery('.cover').fadeIn();
-	jQuery('.fadeout').fadeOut();
-	open=true;
-}
-
-function closeMenu() {
-
-	console.log("closeMenu called");
-	jQuery('.cover').animate({
-		left: '0%'
-	});
-	jQuery('.mobile-menu-bar').animate({
-		left: '0%'
-	});
-	jQuery('.off-screen-left').animate({
-		left: '-80%'
-	});
-	jQuery('.outer').css({
-		position: 'relative'
-	});
-	jQuery('body').scrollTop(scrollPos);
-	jQuery(".body").off("touchmove", false);
-	jQuery('.cover').fadeOut();
-	jQuery('.fadeout').fadeIn();
-	open=false;
-}
+						console.log("closeMenu called");
+						jQuery('.cover').animate({
+							left: '0%'
+						});
+						jQuery('.mobile-menu-bar').animate({
+							left: '0%'
+						});
+						jQuery('.off-screen-left').animate({
+							left: '-80%'
+						});
+						jQuery('.outer').css({
+							position: 'relative'
+						});
+						jQuery('body').scrollTop(scrollPos);
+						jQuery(".body").off("touchmove", false);
+						jQuery('.cover').fadeOut();
+						jQuery('.fadeout').fadeIn();
+						open = false;
+					}
